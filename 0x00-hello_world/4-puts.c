e9b67b74e3e8b6f3af347acebad43f84a0563cb8
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-17 07:23:51 -0700

    uplode C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/* main the entry point of the programme
-*/
 
-int main (void)
-{ 
-	/* the diffrance between puts and printf , puts desplay only strings */ 
-	puts ("\"Programming is like building a multilingual puzzle");
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
-
 }
