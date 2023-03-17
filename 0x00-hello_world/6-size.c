f259b68304bb83880029f4a54ed059ee57d3cc07
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-17 07:36:34 -0700

    uplode C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry poinr
- * Return : always return 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	
 	int a;
 	long int b;
 	long long int c;
@@ -19,5 +19,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-
 }
