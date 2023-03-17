23ce9809eabac923963f6bab010aedecdf84f288
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-17 07:29:59 -0700

    uplode C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/*
- * main is the intry point for the programme 
- **/
-int main (void)
-{ 
+/**
+ * main - Entry point
+ *
+ *
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
