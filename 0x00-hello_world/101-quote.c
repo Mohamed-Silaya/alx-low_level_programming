18540e63d7f3d0d8b4e5d4b9f553d5e6c9ae7ed0
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-17 07:57:09 -0700

    uplode C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
+ *
+ * write : is system utility
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
