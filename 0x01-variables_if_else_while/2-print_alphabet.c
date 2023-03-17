b560ceb01d8bfbb7396cf55729dccf101be9e9de
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-17 09:32:34 -0700

    C -variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char first;
+	for (first ='a'; first <='z';first ++)
+	{
+		putchar(first);
+		putchar("\n");
+	}
+}	
---
+++ 0x01-variables_if_else_while/3-print_alphabetd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+    	char low,upper;
+	for (low ='a' ;low <='z'; low++)
+	{
+		putchar(low);
+	}
+	for (upper ='A';upper='Z';upper++)
+	{
+		putchar(upper);
+		putchar("\n");
+	
+	}
+}
