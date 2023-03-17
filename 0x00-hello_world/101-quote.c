c909891ae8eba90d69fbac8ba3ef5a388a718a60
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-17 08:09:47 -0700

    uplode C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - Entry point
@@ -10,7 +9,7 @@
  */
 int main(void)
 {
-write (2 ,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
+	write (2 ,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
 
 	return (1);
 }
