d476caddf1d5fe67e89b6f376c804cde71de6f36
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-17 06:49:16 -0700

    uplode C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* main the entry point of the programme
+*/
+
+int main (void)
+{ 
+	/* the diffrance between puts and printf , puts desplay only strings */ 
+	puts ("Programming is like building a multilingual puzzle,\n");
+	return (0);
+
+
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf (sizeof(char));
+        printf (sizeof(int));
+	printf (sizeof(float));
+	printf (sizeof(long));
+
+
+	return(0);
+
+}
