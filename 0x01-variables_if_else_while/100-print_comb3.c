29b393dbd9d1b83050b6d6b77fe08473c926de2c
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-19 15:13:30 -0700

    C -variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
     int i, j;
@@ -22,12 +22,13 @@ int main(void)
             {
                 continue;
             }
-		
+
             putchar(',');
             putchar(' ');
-		
         }
     }
+
+    putchar('\n');
 
     return (0);
 }
