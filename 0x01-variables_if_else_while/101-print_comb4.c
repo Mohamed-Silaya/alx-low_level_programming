36adb6fa7111b1f642990ba897b6aa1ff657a5d5
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-19 15:59:41 -0700

    C -variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,7 +20,9 @@ int main(void)
                 putchar(k + '0');
 
                 if (i == 7 && j == 8 && k == 9)
+                {
                     continue;
+                }
 
                 putchar(',');
                 putchar(' ');
