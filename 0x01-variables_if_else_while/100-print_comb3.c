8c5a739f14773526124a0207989956d08456fa51
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-19 15:19:13 -0700

    C -variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -9,19 +7,17 @@
  */
 int main(void)
 {
-int i, j;
+    int i, j;
 
-for (i = 0; i <= 8; i++)
-{
+    for (i = 0; i <= 8; i++)
+    {
         for (j = i + 1; j <= 9; j++)
         {
             putchar(i + '0');
             putchar(j + '0');
 
             if (i == 8 && j == 9)
-            {
                 continue;
-            }
 
             putchar(',');
             putchar(' ');
@@ -29,5 +25,6 @@ for (i = 0; i <= 8; i++)
     }
 
     putchar('\n');
-    return (0);
+
+    return 0;
 }
