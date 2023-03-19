01cf90c2a8c766bf543a9f23ba18ed7ccf69dd0e
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-19 14:40:12 -0700

    C -variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int i, j;
+
+    for (i = 0; i <= 8; i++)
+    {
+        for (j = i + 1; j <= 9; j++)
+        {
+            putchar(i + '0');
+            putchar(j + '0');
+
+            if (i == 8 && j == 9)
+            {
+                continue;
+            }
+
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    return (0);
+}
