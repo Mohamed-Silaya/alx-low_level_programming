a16aa3474d62cca50d391f75aa11f1ed403afc50
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-03-20 08:51:20 -0700

    C -variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,11 +13,11 @@ for (i = 0; i <= 7; i++)
 {
 	for (j = i + 1; j <= 8; j++)
 	{
-        	for (k = j + 1; k <= 9; k++)
-           	 {
-               	 putchar(i + '0');
-              	 putchar(j + '0');
-               	 putchar(k + '0');
+        for (k = j + 1; k <= 9; k++)
+        {
+           putchar(i + '0');
+           putchar(j + '0');
+           putchar(k + '0');
 
                 if (i == 7 && j == 8 && k == 9)
                     continue;
