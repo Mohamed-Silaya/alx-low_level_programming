bd0039c6c4736ad5c8c77d571e189ce9b083ad8d
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-04-02 16:27:34 -0700

    Pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <unistd>
+
 void _puts(char *str)
 {
 	int x;
 	for ( x = 0; str[x] != '\0'; x++ )
 	{
-		putchar(*str[x]);
+		_putchar(str[x]);
 	}
 
-	putchar('\n');
+	_putchar('\n');
 }
