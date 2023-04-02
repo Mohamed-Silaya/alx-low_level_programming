865982de4febcb5595ea2f9cb810071343c9db14
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-04-02 16:17:46 -0700

    Pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,12 @@
 #include "main.h"
+
+void _puts(char *str)
+{
+	int x;
+	for ( x = 0; str[x] != '\0'; x++ )
+	{
+		_putchar(*str[x]);
+	}
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+
+#endif
