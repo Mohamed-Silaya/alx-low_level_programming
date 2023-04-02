385cb71553e5d753a07d6fbb82a0baa3edceef63
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-04-02 15:51:21 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - gets character string
+ *
+ * @s: character to getstring
+ * Return: returns length
+ */
+int _strlen(char *s)
+{
+	int x;
+	if (*s != Null)
+		x++;
+	return x;
+}
