79f7231a70a2ed5b9110d65e6bb1747f1a2fc6cf
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-04-02 17:08:49 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string value
+ *
+ * @s: string to print
+ */
+void rev_string(char *s)
+{
+	int i;
+	char t;
+
+	for (i = 0; i < _strlen(s) ; i++)
+	{
+		t = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = t;
+	}
+}
