187201902af6dceb09eb36372c0a511fb694ac30
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-04-04 17:24:43 -0700

    0x06. C - More pointer

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * print_number - prints an integer;
- * @n: integer to be printed;
- */
-void print_number(int n)
+int main(void)
 {
-	unsigned int n1;
+  int n;
+  int a[5];
+  int *p;
 
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-		print_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
