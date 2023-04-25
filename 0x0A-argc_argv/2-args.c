e35c3814a97632d3bf1ddeffc171b7a4c1817fda
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-04-25 01:04:25 -0700

    arg

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
+ * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -10,8 +10,12 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 0;
-	for (i = 1; i < argc; i++)
-		printf("%s\n",argv[i]);
-	return 0;
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
 }
