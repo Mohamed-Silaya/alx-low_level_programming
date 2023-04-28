04d4b34ce0d83dd61fdcc9d5feee8a2b30a2848e
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2023-04-27 22:12:21 -0700

    malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,68 +1,99 @@
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * multiply - multiplies two positive integers represented as strings
- * @s1: first integer as a string
- * @s2: second integer as a string
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: a pointer to the product as a string, or NULL on failure
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-char *multiply(const char *s1, const char *s2)
+int is_digit(char *s)
 {
-    size_t len1 = 0, len2 = 0, len = 0, i, j;
-    int carry = 0, digit1 = 0, digit2 = 0, product = 0;
-    char *result = NULL;
+	int i = 0;
 
-    if (s1 == NULL || s2 == NULL) {
-        return NULL;
-    }
-    while (s1[len1] != '\0' && isdigit(s1[len1])) {
-        len1++;
-    }
-    while (s2[len2] != '\0' && isdigit(s2[len2])) {
-        len2++;
-    }
-    if (len1 == 0 || len2 == 0) {
-        return NULL;
-    }
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
-    len = len1 + len2 + 1;
-    result = malloc(len);
-    if (result == NULL) {
-        return NULL;
-    }
-    for (i = 0; i < len - 1; i++) {
-        result[i] = '0';
-    }
-    result[len - 1] = '\0';
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
 
-    for (i = len1; i > 0; i--) {
-        digit1 = s1[i - 1] - '0';
-        carry = 0;
-        for (j = len2; j > 0; j--) {
-            digit2 = s2[j - 1] - '0';
-            product = digit1 * digit2 + carry + result[i + j - 2] - '0';
-            carry = product / 10;
-            result[i + j - 2] = product % 10 + '0';
-        }
-        if (carry > 0) {
-            result[i + j - 2] += carry;
-        }
-    }
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
-    i = 0;
-    while (result[i] == '0' && result[i + 1] != '\0') {
-        i++;
-    }
-    if (i > 0) {
-        len -= i;
-        for (j = 0; j < len; j++) {
-            result[j] = result[j + i];
-        }
-    }
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
 
-    return result;
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
